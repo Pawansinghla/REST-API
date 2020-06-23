37f2fb197c628816b78f1c897de2fb19cf0183d9
pawan singhla <pawansinghla300@gmail.com>
2020-06-23 19:56:21 +0530

    Express REST API with MongoDB and Mongoose Part 1

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,24 +1,42 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 
+const mongoose =require('mongoose');
+
+const Dishes=require('../models/dishes');
+
 const dishRouter=express.Router();
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
 
-.all((req,res,next)=>{ // it will execute by default first
-    res.statusCode=200;
-    res.setHeader('Content-type','text/plain');
-    next();//  it means it will continue to  look for addition spceification down below 
-  })
+// .all((req,res,next)=>{ // it will execute by default first
+//     res.statusCode=200;
+//     res.setHeader('Content-type','text/plain');
+//     next();//  it means it will continue to  look for addition spceification down below 
+//   })
   
   .get((req,res,next)=>{//modify res,req come from above
-    res.end('will send all the dishes to you');
+   Dishes.find({})
+    .then((dishes)=>{
+      res.statusCode=200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(dishes);
+
+    },(err)=>next(err))
+    .catch((err)=>next(err));
   })
   
 .post((req,res,next)=>{//same
-    res.end('Will add the dish: '+req.body.name+ 
-    ' With details: '+req.body.description);
+   Dishes.create(req.body)
+   .then((dish)=>{
+     console.log('Dish Created',dish);
+     res.statusCode=200;
+     res.setHeader('Content-Type','application/json');
+     res.json(dish);
+   },(err)=>next(err))
+
+   .catch((err)=>next(err));
   })
   
   .put((req,res,next)=>{
@@ -27,27 +45,61 @@ dishRouter.route('/')
   
   })
 .delete((req,res,next)=>{
-    res.end('Deleting all the dishes!');
+    Dishes.remove({})
+    .then((resp)=>{
+      res.statusCode=200;
+     res.setHeader('Content-Type','application/json');
+     res.json(resp);
+   },(err)=>next(err))
+
+   .catch((err)=>next(err));
   
   });
 
-  dishRouter.route('/:dishId').all((req,res,next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-  })
+  dishRouter.route('/:dishId')
+  // .all((req,res,next)=>{
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type','text/plain');
+  //   next();
+  // })
   .get((req,res,next)=>{
-    res.end('will send details of the dish with id : ' + req.params.dishId);
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+      res.statusCode=200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(dish);
+
+    },(err)=>next(err))
+    .catch((err)=>next(err));
   })
+
   .post((req,res,next)=>{
     res.statusCode = 403;
     res.end('post to /dishes/' + req.params.dishId + ' is forbidden');
   })
   .put((req,res,next)=>{
-    res.end('dish with id : ' + req.params.dishId + ' will be updated with name: ' + req.body.name +' and description: '+req.body.description);
+    Dishes.findByIdAndUpdate(req.param.dishId,{
+      $set:req.body
+    },{new :true})
+    .then((dish)=>{
+      res.statusCode=200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(dish);
+
+    },(err)=>next(err))
+    .catch((err)=>next(err));
   })
+
   .delete((req,res,next)=>{
-    res.end('deleting dish with id : '+ req.params.dishId);
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then((resp)=>{
+      res.statusCode=200;
+     res.setHeader('Content-Type','application/json');
+     res.json(resp);
+   },(err)=>next(err))
+
+   .catch((err)=>next(err));
+   
   });
 
 
